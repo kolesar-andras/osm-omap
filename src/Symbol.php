b6f5ac20ccf7f12284cde8184d017f417b6ef0b9
Kolesár András <kolesar@openstreetmap.hu>
2016-02-27 07:07:39 +0100

    decode all osm tags

--- src/Symbol.php
+++ src/Symbol.php
@@ -19,27 +19,67 @@ class Symbol {
      */
     static function getSymbol($feature) {
 
-        $tags = $feature['properties'];
+        $tags = static::getTags($feature['properties']);
 
-        if (in_array($tags['highway'], ['track', 'unsurfaced', 'bridleway', 'cycleway']))
+        if (in_array(@$tags['highway'], ['track', 'unsurfaced', 'bridleway', 'cycleway']))
             return 93; // code="505" name="Vehicle track"
 
-        if (in_array($tags['highway'], ['footway']))
+        if (in_array(@$tags['highway'], ['footway']))
             return 94; // code="506" name="Footpath"
 
-        if (in_array($tags['highway'], ['path']))
+        if (in_array(@$tags['highway'], ['path']))
             return 95; // code="507" name="Small path"
 
-        if (in_array($tags['highway'], ['tertiary', 'unclassified', 'residential', 'service']))
+        if (in_array(@$tags['highway'], ['tertiary', 'unclassified', 'residential', 'service']))
             return 90; // code="503" name="Minor road"
 
-        if (in_array($tags['highway'], ['primary', 'secondary']))
+        if (in_array(@$tags['highway'], ['primary', 'secondary']))
             return 88; // code="502" name="Major road, minimum width"
 
-        if (in_array($tags['waterway'], ['stream', 'ditch', 'canal']))
+        if (in_array(@$tags['waterway'], ['stream', 'ditch', 'canal']))
             return 50; // code="305" name="Crossable watercourse"
+
+        if (in_array(@$tags['railway'], ['rail']))
+            return 99; // code="515" name="Railway"
 
         return -3; // magic number for unknown symbol
 
+    }
+
+    /**
+     * Get tags of feature
+     *
+     * GDAL OGR driver handles OSM tags in a special way.
+     * Uses a config file named osmconf.ini for mapping tags.
+     * Default configuration creates columns for some keys,
+     * other tags are placed to other_tags column.
+     *
+     * Can be configured to put all tags to all_tags column.
+     *
+     * These fields are filled with key-value pairs
+     * encoded with PostgreSQL hstore syntax:
+     * "key" => "value", [...]
+     *
+     * This method finds these fields and decodes tags.
+     *
+     * @param $tags tags of feature
+     * @return associative array of tags
+     */
+    static function getTags($tags) {
+        foreach (['other_tags', 'all_tags'] as $column) {
+            if (!isset($tags[$column])) continue;
+            $array = static::decodeHstore($tags[$column]);
+            if (!is_array($array)) continue;
+            $tags = array_merge($tags, $array);
+        }
+        return $tags;
+    }
+
+    /**
+     * Convert PostgreSQL hstore key-value pairs to associative array
+     * by simply converting to JSON object
+     */
+    static function decodeHstore($hstore) {
+        return json_decode('{' . str_replace('"=>"', '":"', $hstore) . '}', true);
     }
 }
